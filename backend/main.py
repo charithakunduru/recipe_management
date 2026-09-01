9049de502b947deb349f6469010e84f1194d43f9
charithakunduru <charithakunduru2005@gmail.com>
2026-09-01 23:45:50 +0530

    Fix CORS for local frontend

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,10 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://127.0.0.1:5501",
+        "http://localhost:5501"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
